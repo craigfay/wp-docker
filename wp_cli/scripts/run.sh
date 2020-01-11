1ec0ca2a2193f06f3c83150925a2681d07a8911c
craig fay <craigfay1@gmail.com>
2020-01-11 17:07:03 -0600

    Add wp_cli script

---
+++ wp_cli/scripts/run.sh
@@ -0,0 +1,17 @@
+
+
+wp core install \
+    --url=wordpress \
+    --title=wordpress \
+    --admin_user=admin \
+    --admin_password=admin \
+    --admin_email=admin@admin.com
+
+wp config create \
+    --dbhost=db \
+    --dbname=wordpress \
+    --dbuser=wordpress \
+    --dbpass=wordpress \
+    --skip-check \
+    --force
+
